08d8837b0a91cc48c64e699e357d5ace6c9064da
omega1234az <supakit0920@gmail.com>
2025-02-06 10:53:30 +0700

    add readme -- style

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-white">
       <div className="p-6 bg-white shadow-lg rounded-xl text-center text-gray-900">
         <h1 className="text-2xl font-bold">66026167</h1>
-        <p className="text-lg mt-2">นายศุภกิตติ์ ราญพล</p>
+        <p className="text-lg mt-2">-- นายศุภกิตติ์ ราญพล  --</p>
       </div>
     </div>
 
